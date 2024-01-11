b42e6e3a051791d359e0fffe934d888e493bb37f
Mariam019 <80398941+Mariam019@users.noreply.github.com>
2024-01-12 00:10:48 +0100

    Update ServiceEns.java

--- TP_POO/phase1/src/Services/ServiceEns.java
+++ TP_POO/phase1/src/Services/ServiceEns.java
@@ -3,17 +3,41 @@ import classes.Enseignant;
 import classes.Departement;
 import java.util.ArrayList;
 
-public class ServiceEns {
-    public static Enseignant addEns(String nom, String prenom, String email, String grade, Departement dep){
+ public static Enseignant addEns(String nom, String prenom, String email, String grade, Departement... dep){
+        Enseignant enseignant = new Enseignant();
+        enseignant.setNom(nom);
+        enseignant.setPrenom(prenom);
+        enseignant.setEmail(email);
+        enseignant.setGrade(grade);
+        if (dep.length > 0){
+            enseignant.setDepartement(dep[0]);
+        }
+        Fichiers.enseignants.add(enseignant);
         return  new Enseignant();
     }
 
-    public static Enseignant updateEns(int id, String nom, String prenom, String email, String grade, Departement dep){
+    public static Enseignant updateEns(int id, String nom, String prenom, String email, String grade, Departement... dep){
+        for (Enseignant enseignant : Fichiers.enseignants) {
+            if (enseignant.getId() == id) {
+                enseignant.setNom(nom);
+                if (dep.length > 0){
+                    enseignant.setDepartement(dep[0]);
+                }
+                return enseignant;
+            }
+        }
+
         return  new Enseignant();
     }
-    public static ArrayList<Enseignant> deleteEnsById(int id){
-        return  Fichiers.enseignants;
+    public static boolean deleteEnsById(int id){
+        Enseignant enseignant = getEnsById(id);
+        if (enseignant != null) {
+            return Fichiers.enseignants.remove(enseignant); 
+        }
+        return false; 
+
     }
+
 
     public static Enseignant getEnsById(int id){
         for (Enseignant enseignant : Fichiers.enseignants) {
