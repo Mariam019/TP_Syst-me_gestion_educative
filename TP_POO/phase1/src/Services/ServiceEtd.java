f938749de84c71476af4d4d9a8730665db78fca0
Mariam019 <80398941+Mariam019@users.noreply.github.com>
2024-01-12 00:13:21 +0100

    Update ServiceEtd.java

--- TP_POO/phase1/src/Services/ServiceEtd.java
+++ TP_POO/phase1/src/Services/ServiceEtd.java
@@ -4,18 +4,48 @@ import classes.Filiere;
 import java.util.ArrayList;
 
 public class ServiceEtd {
-    public static Etudiant addEtd(String nomE, String prenomE, String emailE, int apogee, Filiere filiere){
+        public static Etudiant addEtd(String nomE, String prenomE, String emailE, int apogee, Filiere... filiere){
+        Etudiant etudiant = new Etudiant();
+        etudiant.setNomE(nomE);
+        etudiant.setPrenomE(prenomE);
+        etudiant.setEmailE(emailE);
+        etudiant.setApogee(apogee);
+        if (filiere.length > 0){
+            etudiant.setFiliere(filiere[0]);
+        }
+        Fichiers.etudiants.add(etudiant);
         return  new Etudiant();
     }
 
-    public static Etudiant updateEtd(String nomE, String prenomE, String emailE, int apogee, Filiere filiere){
+    public static Etudiant updateEtd(String nomE, String prenomE, String emailE, int apogee, Filiere... filiere){
+        for (Etudiant etudiant : Fichiers.etudiants) {
+            if (etudiant.getApogee() == apogee) {
+                etudiant.setNomE(nomE);
+                if (filiere.length > 0){
+                    etudiant.setFiliere(filiere[0]);
+                }
+                return etudiant;
+            }
+        }
+
         return  new Etudiant();
     }
-    public static ArrayList<Etudiant> deleteEtdByApogee(int apogee){
-        return  Fichiers.etudiants;
+    public static boolean deleteEtdByApogee(int apogee){
+        Etudiant etudiant = getEtdByApogee(apogee);
+        if (etudiant != null) {
+            return Fichiers.etudiants.remove(etudiant); 
+        }
+        return false; 
+
+
     }
 
-    public static Etudiant getEtdByApogee(int apogee){
+   public static Etudiant getEtdByApogee(int apogee){
+        for(Etudiant etudiant : Fichiers.etudiants){
+            if(etudiant.getApogee() == apogee){
+                return etudiant;
+            }
+        }
         return  new Etudiant();
     }
 
