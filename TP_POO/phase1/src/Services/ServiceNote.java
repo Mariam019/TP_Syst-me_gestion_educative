74fc5a22473ffbda88fed8d6433c4e6d7e159c22
Mariam019 <80398941+Mariam019@users.noreply.github.com>
2024-01-12 00:17:37 +0100

    Update ServiceNote.java

--- TP_POO/phase1/src/Services/ServiceNote.java
+++ TP_POO/phase1/src/Services/ServiceNote.java
@@ -6,16 +6,45 @@ import classes.Note;
 import java.util.ArrayList;
 
 public class ServiceNote {
-    public static Note addNote(float note, Etudiant etudiant, Module module){
+  public static Note addNote(float note, Etudiant etudiant, Module module){
+        Note note1  = new Note();
+        note1.setNote(note);
+        note1.setEtudiant(etudiant);
+        note1.setModule(module);
+
+        Fichiers.notes.add(note1);
+
         return new Note();
     }
     public static Note updateNote(float note, Etudiant etudiant, Module module){
+        for(Note note1 : Fichiers.notes){
+            if(note1.getNote() == note){
+                note1.setEtudiant(etudiant);
+                note1.setModule(module);
+
+                return note1;
+            }
+        }
         return new Note();
     }
     public static ArrayList<Note> deleteNoteById(int id){
-        return  Fichiers.notes;
+        ArrayList<Note> notesToRemove = new ArrayList<>();
+        for(Note note1 : Fichiers.notes){
+            if(note1.getNote() == note1.getNote()){
+                notesToRemove.add(note1);
+            }
+        }
+        Fichiers.modules.removeAll(notesToRemove);
+        return notesToRemove;
+
     }
-    public static Note getNoteById(int id){
+
+    public static Note getNoteByNote(float note){
+        for(Note note1 : Fichiers.notes){
+            if (note1.getNote() == note) {
+                return note1;
+            }
+        }
         return  new Note();
     }
 
