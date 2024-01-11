d20cd5ffe68cfef7673c4693eca5dd13d5438b6e
Mariam019 <80398941+Mariam019@users.noreply.github.com>
2024-01-12 00:14:14 +0100

    Update ServiceFil.java

--- TP_POO/phase1/src/Services/ServiceFil.java
+++ TP_POO/phase1/src/Services/ServiceFil.java
@@ -8,18 +8,46 @@ import java.util.ArrayList;
 
 
 public class ServiceFil {
-    public static Filiere addFiliere(String nomFil, Enseignant responsable, Departement dep) {
+    public static Filiere addFiliere(String nomFil, Enseignant responsable, Departement... dep) {
+        Filiere filiere = new Filiere();
+        filiere.setNomFil(nomFil);
+
+        filiere.setResponsable(responsable);
+
+        if (dep.length > 0){
+            filiere.setDepartement(dep[0]);
+        }
+        Fichiers.filieres.add(filiere);
         return  new Filiere();
     }
 
-    public static Filiere updateFiliere(int id , String nomFil, Enseignant responsable, Departement dep){
+    public static Filiere updateFiliere(int id , String nomFil, Enseignant responsable, Departement... dep){
+        for (Filiere filiere: Fichiers.filieres) {
+            if (filiere.getId() == id) {
+                filiere.setNomFil(nomFil);
+                if (dep.length > 0){
+                    filiere.setDepartement(dep[0]);
+                }
+                return filiere;
+            }
+        }
         return  new Filiere();
     }
-    public static ArrayList<Filiere> deleteFiliereById(int id){
-        return  Fichiers.filieres;
+    public static boolean deleteFiliereById(int id){
+        Filiere filiere = getFiliereById(id);
+        if (filiere != null) {
+            return Fichiers.departements.remove(filiere); // Remove the department
+        }
+        return false;
+
     }
 
     public static Filiere getFiliereById(int id){
+        for(Filiere filiere : Fichiers.filieres){
+            if(filiere.getId() == id){
+                return filiere;
+            }
+        }
         return  new Filiere();
     }
 
